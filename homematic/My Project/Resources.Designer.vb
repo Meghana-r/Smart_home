93db2644c8f8010b78d60d86c5b8a8cd130f36b0
Meghana-r <30573380+Meghana-r@users.noreply.github.com>
2017-10-09 21:36:30 +0530

    Added blueprint

--- homematic/My Project/Resources.Designer.vb
+++ homematic/My Project/Resources.Designer.vb
@@ -63,6 +63,26 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property blueprint() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("blueprint", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property blueprint1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("blueprint1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property fridgeoff() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("fridgeoff", resourceCulture)
