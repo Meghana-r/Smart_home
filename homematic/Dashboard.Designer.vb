f7ccfd7e8dcad3728bf7a04b727b00f80eedc381
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2017-10-11 07:21:37 +0530

    dashboard sieze = normal

--- homematic/Dashboard.Designer.vb
+++ homematic/Dashboard.Designer.vb
@@ -23,145 +23,139 @@ Partial Class Dashboard
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()>
 	Private Sub InitializeComponent()
-		Me.Panel1 = New System.Windows.Forms.Panel()
-		Me.R3btn = New System.Windows.Forms.Button()
-		Me.R2btn = New System.Windows.Forms.Button()
-		Me.Bathbtn = New System.Windows.Forms.Button()
-		Me.Kitchbtn = New System.Windows.Forms.Button()
-		Me.MasterBTN = New System.Windows.Forms.Button()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.Panel1.SuspendLayout()
-		Me.SuspendLayout()
-		'
-		'Panel1
-		'
-		Me.Panel1.BackColor = System.Drawing.Color.Transparent
-		Me.Panel1.Controls.Add(Me.R3btn)
-		Me.Panel1.Controls.Add(Me.R2btn)
-		Me.Panel1.Controls.Add(Me.Bathbtn)
-		Me.Panel1.Controls.Add(Me.Kitchbtn)
-		Me.Panel1.Controls.Add(Me.MasterBTN)
-		Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
-		Me.Panel1.Location = New System.Drawing.Point(0, 487)
-		Me.Panel1.Name = "Panel1"
-		Me.Panel1.Size = New System.Drawing.Size(769, 42)
-		Me.Panel1.TabIndex = 0
-		'
-		'R3btn
-		'
-		Me.R3btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.R3btn = New System.Windows.Forms.Button()
+        Me.R2btn = New System.Windows.Forms.Button()
+        Me.Bathbtn = New System.Windows.Forms.Button()
+        Me.Kitchbtn = New System.Windows.Forms.Button()
+        Me.MasterBTN = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.R3btn)
+        Me.Panel1.Controls.Add(Me.R2btn)
+        Me.Panel1.Controls.Add(Me.Bathbtn)
+        Me.Panel1.Controls.Add(Me.Kitchbtn)
+        Me.Panel1.Controls.Add(Me.MasterBTN)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 487)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(769, 42)
+        Me.Panel1.TabIndex = 0
+        '
+        'R3btn
+        '
+        Me.R3btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.R3btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.R3btn.Dock = System.Windows.Forms.DockStyle.Left
+        Me.R3btn.FlatAppearance.BorderSize = 0
+        Me.R3btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.R3btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.R3btn.ForeColor = System.Drawing.Color.White
+        Me.R3btn.Location = New System.Drawing.Point(612, 0)
+        Me.R3btn.Name = "R3btn"
+        Me.R3btn.Size = New System.Drawing.Size(153, 42)
+        Me.R3btn.TabIndex = 4
+        Me.R3btn.Text = "Room3"
+        Me.R3btn.UseVisualStyleBackColor = True
+        '
+        'R2btn
+        '
+        Me.R2btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.R2btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.R2btn.Dock = System.Windows.Forms.DockStyle.Left
+        Me.R2btn.FlatAppearance.BorderSize = 0
+        Me.R2btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.R2btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.R2btn.ForeColor = System.Drawing.Color.White
+        Me.R2btn.Location = New System.Drawing.Point(459, 0)
+        Me.R2btn.Name = "R2btn"
+        Me.R2btn.Size = New System.Drawing.Size(153, 42)
+        Me.R2btn.TabIndex = 3
+        Me.R2btn.Text = "Room2"
+        Me.R2btn.UseVisualStyleBackColor = True
+        '
+        'Bathbtn
+        '
+        Me.Bathbtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Bathbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Bathbtn.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Bathbtn.FlatAppearance.BorderSize = 0
+        Me.Bathbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Bathbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bathbtn.ForeColor = System.Drawing.Color.White
+        Me.Bathbtn.Location = New System.Drawing.Point(306, 0)
+        Me.Bathbtn.Name = "Bathbtn"
+        Me.Bathbtn.Size = New System.Drawing.Size(153, 42)
+        Me.Bathbtn.TabIndex = 2
+        Me.Bathbtn.Text = "Bathroom"
+        Me.Bathbtn.UseVisualStyleBackColor = True
+        '
+        'Kitchbtn
+        '
+        Me.Kitchbtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Kitchbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Kitchbtn.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Kitchbtn.FlatAppearance.BorderSize = 0
+        Me.Kitchbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Kitchbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Kitchbtn.ForeColor = System.Drawing.Color.White
+        Me.Kitchbtn.Location = New System.Drawing.Point(153, 0)
+        Me.Kitchbtn.Name = "Kitchbtn"
+        Me.Kitchbtn.Size = New System.Drawing.Size(153, 42)
+        Me.Kitchbtn.TabIndex = 1
+        Me.Kitchbtn.Text = "Kitchen"
+        Me.Kitchbtn.UseVisualStyleBackColor = True
+        '
+        'MasterBTN
+        '
+        Me.MasterBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MasterBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.MasterBTN.Dock = System.Windows.Forms.DockStyle.Left
+        Me.MasterBTN.FlatAppearance.BorderSize = 0
+        Me.MasterBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MasterBTN.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MasterBTN.ForeColor = System.Drawing.Color.White
+        Me.MasterBTN.Location = New System.Drawing.Point(0, 0)
+        Me.MasterBTN.Name = "MasterBTN"
+        Me.MasterBTN.Size = New System.Drawing.Size(153, 42)
+        Me.MasterBTN.TabIndex = 0
+        Me.MasterBTN.Text = "Master Bedroom"
+        Me.MasterBTN.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.ImageAlign = System.Drawing.ContentAlignment.BottomRight
+        Me.Label1.Location = New System.Drawing.Point(306, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 39)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Dashboard"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'Dashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(769, 529)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Dashboard"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
 
-		Me.R3btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-		Me.R3btn.Dock = System.Windows.Forms.DockStyle.Left
-		Me.R3btn.FlatAppearance.BorderSize = 0
-		Me.R3btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.R3btn.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.R3btn.ForeColor = System.Drawing.Color.White
-		Me.R3btn.Location = New System.Drawing.Point(612, 0)
-		Me.R3btn.Name = "R3btn"
-		Me.R3btn.Size = New System.Drawing.Size(153, 42)
-		Me.R3btn.TabIndex = 4
-		Me.R3btn.Text = "Room3"
-		Me.R3btn.UseVisualStyleBackColor = True
-		'
-		'R2btn
-		'
-		Me.R2btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+    End Sub
 
-		Me.R2btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-		Me.R2btn.Dock = System.Windows.Forms.DockStyle.Left
-		Me.R2btn.FlatAppearance.BorderSize = 0
-		Me.R2btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.R2btn.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.R2btn.ForeColor = System.Drawing.Color.White
-		Me.R2btn.Location = New System.Drawing.Point(459, 0)
-		Me.R2btn.Name = "R2btn"
-		Me.R2btn.Size = New System.Drawing.Size(153, 42)
-		Me.R2btn.TabIndex = 3
-		Me.R2btn.Text = "Room2"
-		Me.R2btn.UseVisualStyleBackColor = True
-		'
-		'Bathbtn
-		'
-		Me.Bathbtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-
-		Me.Bathbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-		Me.Bathbtn.Dock = System.Windows.Forms.DockStyle.Left
-		Me.Bathbtn.FlatAppearance.BorderSize = 0
-		Me.Bathbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.Bathbtn.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Bathbtn.ForeColor = System.Drawing.Color.White
-		Me.Bathbtn.Location = New System.Drawing.Point(306, 0)
-		Me.Bathbtn.Name = "Bathbtn"
-		Me.Bathbtn.Size = New System.Drawing.Size(153, 42)
-		Me.Bathbtn.TabIndex = 2
-		Me.Bathbtn.Text = "Bathroom"
-		Me.Bathbtn.UseVisualStyleBackColor = True
-		'
-		'Kitchbtn
-		'
-		Me.Kitchbtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-
-		Me.Kitchbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-		Me.Kitchbtn.Dock = System.Windows.Forms.DockStyle.Left
-		Me.Kitchbtn.FlatAppearance.BorderSize = 0
-		Me.Kitchbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.Kitchbtn.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Kitchbtn.ForeColor = System.Drawing.Color.White
-		Me.Kitchbtn.Location = New System.Drawing.Point(153, 0)
-		Me.Kitchbtn.Name = "Kitchbtn"
-		Me.Kitchbtn.Size = New System.Drawing.Size(153, 42)
-		Me.Kitchbtn.TabIndex = 1
-		Me.Kitchbtn.Text = "Kitchen"
-		Me.Kitchbtn.UseVisualStyleBackColor = True
-		'
-		'MasterBTN
-		'
-		Me.MasterBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-
-		Me.MasterBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-		Me.MasterBTN.Dock = System.Windows.Forms.DockStyle.Left
-		Me.MasterBTN.FlatAppearance.BorderSize = 0
-		Me.MasterBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.MasterBTN.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.MasterBTN.ForeColor = System.Drawing.Color.White
-		Me.MasterBTN.Location = New System.Drawing.Point(0, 0)
-		Me.MasterBTN.Name = "MasterBTN"
-		Me.MasterBTN.Size = New System.Drawing.Size(153, 42)
-		Me.MasterBTN.TabIndex = 0
-		Me.MasterBTN.Text = "Master Bedroom"
-		Me.MasterBTN.UseVisualStyleBackColor = True
-		'
-		'Label1
-		'
-		Me.Label1.BackColor = System.Drawing.Color.Transparent
-		Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-		Me.Label1.Font = New System.Drawing.Font("Roboto Light", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label1.ForeColor = System.Drawing.Color.White
-		Me.Label1.ImageAlign = System.Drawing.ContentAlignment.BottomRight
-		Me.Label1.Location = New System.Drawing.Point(306, 25)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(186, 39)
-		Me.Label1.TabIndex = 1
-		Me.Label1.Text = "Dashboard"
-		Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
-		'
-		'Dashboard
-		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.BackColor = System.Drawing.Color.White
-		Me.ClientSize = New System.Drawing.Size(769, 529)
-		Me.Controls.Add(Me.Label1)
-		Me.Controls.Add(Me.Panel1)
-		Me.Name = "Dashboard"
-		Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-		Me.Panel1.ResumeLayout(False)
-		Me.ResumeLayout(False)
-
-	End Sub
-
-	Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel1 As Panel
 	Friend WithEvents MasterBTN As Button
 	Friend WithEvents R3btn As Button
 	Friend WithEvents R2btn As Button
