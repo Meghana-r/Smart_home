a983aa0b38c13b3951990727d0f824826f0b197a
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2017-10-17 05:39:27 +0530

    database file updated

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,38 +3,37 @@
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room1Light1', 'Room1', 15, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room1Light2', 'Room1', 30, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room1Light3', 'Room1', 45, 0)
-insert into billTable (NAME, ROOM, RATE, TIME) values ('Room1Pw1', 'Room1', 15, 0)
 
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room2Light1', 'Room2', 15, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room2Light2', 'Room2', 30, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room2Light3', 'Room2', 45, 0)
-insert into billTable (NAME, ROOM, RATE, TIME) values ('Room2Pw1', 'Room2', 30, 0)
 
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room3Light1', 'Room3', 15, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room3Light2', 'Room3', 30, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room3Light3', 'Room3', 45, 0)
-insert into billTable (NAME, ROOM, RATE, TIME) values ('Room3Pw1', 'Room3', 45, 0)
 
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room4Light1', 'Room4', 15, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room4Light2', 'Room4', 30, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room4Light3', 'Room4', 45, 0)
-insert into billTable (NAME, ROOM, RATE, TIME) values ('Room4Pw1', 'Room4', 15, 0)
 
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room5Light1', 'Room5', 15, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room5Light2', 'Room5', 30, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room5Light3', 'Room5', 45, 0)
-insert into billTable (NAME, ROOM, RATE, TIME) values ('Room5Pw1', 'Room5', 1000, 0)
 
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room6Light1', 'Room6', 15, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room6Light2', 'Room6', 30, 0)
 insert into billTable (NAME, ROOM, RATE, TIME) values ('Room6Light3', 'Room6', 45, 0)
-insert into billTable (NAME, ROOM, RATE, TIME) values ('Room6Pw1', 'Room6', 2000, 0)
 
+/* delete the table */
+drop table billTable
 
-
-
-
-
-
+/* display all intems */
 select * from billTable
+
+/* display time */
 select TIME from billTable
+
+/*
+server name : (localdb)\mssqllocaldb
+database name : mylocalDB
+*/
